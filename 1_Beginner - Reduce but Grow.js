a59934e611da340bd872470ef2b32e1206084e28
adhisetiyoko <adhisetiyoko16072002@gmail.com>
2024-02-09 22:45:41 +0700

    Day 1/366

--- 1_Beginner - Reduce but Grow.js
+++ 1_Beginner - Reduce but Grow.js
@@ -1,3 +1,8 @@
+// Adhi Setiyoko
+// 1/366
+// https://www.codewars.com/kata/57f780909f7e8e3183000078
+
+
 // function grow(x){
 //     let result = x[0];
 //     for(let i = 1; i < x.length; i++){
@@ -14,4 +19,6 @@
 
 const grow=x=>eval(x.join("*"))
 
-console.log(grow([1, 2, 3]))
+// console.log(grow([1, 2, 3]));
+// console.log(grow([4, 1, 1, 1, 4]));
+// console.log(grow([2, 2, 2, 2, 2, 2]));
