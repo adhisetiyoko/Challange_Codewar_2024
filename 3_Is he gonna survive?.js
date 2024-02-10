88e017415407a3825c129333a6367ba58b365122
adhisetiyoko <adhisetiyoko16072002@gmail.com>
2024-02-10 23:33:50 +0700

    Day 3/366

--- 3_Is he gonna survive?.js
+++ 3_Is he gonna survive?.js
@@ -1,3 +1,22 @@
 // Adhi Setiyoko
 // 3/366
 // https://www.codewars.com/kata/59ca8246d751df55cc00014c
+
+// function hero(bullets, dragons){
+//     if(bullets/2 >= dragons){
+//         return true;
+//     }else{
+//         return false;
+//     }
+// }
+
+// function hero(bullets, dragons){
+//     return bullets/2 >= dragons;
+// }
+
+// const hero = (bullets, dragons) => bullets/2 >= dragons;
+
+const hero = (_, __) => _/2 >= __;
+
+
+console.log(hero(100, 40));
