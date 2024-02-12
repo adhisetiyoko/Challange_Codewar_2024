c94cd12106cdb504f990f8d7965d32c7f4efb006
adhisetiyoko <adhisetiyoko16072002@gmail.com>
2024-02-12 08:53:45 +0700

    Day 7/366

---
+++ 7_Count by X.js
@@ -0,0 +1,21 @@
+// Adhi Setiyoko
+// 6/366
+// https://www.codewars.com/kata/5513795bd3fafb56c200049e
+
+// function countBy(x, n) {
+//     let z = [];
+//     for(let i = 1; i <= n; i++){
+//         z.push(i * x);
+//     }
+//     return z;
+
+//   }
+
+// function countBy(x, n) {
+//     return [...Array(n)].map((el, i) => ++i * x);
+// }
+
+const countBy = (x, n) => [...Array(n)].map((el, i) => ++i * x);
+
+
+console.log(countBy(2, 10));
