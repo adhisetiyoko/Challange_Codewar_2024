8b7dd5d56afd4639568ccdbf6289de39ef6e5c3a
adhisetiyoko <adhisetiyoko16072002@gmail.com>
2024-02-13 23:10:42 +0700

    Day 10/366

--- 10_Convert a string to an array.js
+++ 10_Convert a string to an array.js
@@ -1,11 +1,13 @@
-
-
-
+// Adhi Setiyoko
+// 10/366
+// https://www.codewars.com/kata/57e76bc428d6fbc2d500036d
 
 function stringToArray(string){
 
 	return string.split(" ");
 
 }
+
+const stringToArray = (string) => string.split(" ");
 
 console.log(stringToArray("I love arrays they are my favorite"));
