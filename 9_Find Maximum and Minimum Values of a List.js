0602bfe9ed91c4c79186df1c9257f4b9765ee4e3
adhisetiyoko <adhisetiyoko16072002@gmail.com>
2024-02-13 21:30:22 +0700

    Day 9/366

---
+++ 9_Find Maximum and Minimum Values of a List.js
@@ -0,0 +1,43 @@
+// Adhi Setiyoko
+// 8/366
+// https://www.codewars.com/kata/577a98a6ae28071780000989
+
+// var min = function(list){
+    
+//     let minValue = list[0];
+  
+//     for (let i = 1; i < list.length; i++) {
+//         if (minValue > list[i]){
+//             minValue = list[i];
+//         }
+        
+//     }
+
+//     return minValue;
+// }
+
+// var max = function(list){
+
+//     let maxValue = list[0];
+
+//     for (let i = 1; i < list.length; i++) {
+//             if (maxValue < list[i]){
+//                 maxValue = list[i];
+//             }
+            
+//         }
+        
+//         return maxValue;
+// }
+
+// const min = (list) => Math.min(...list);
+
+// const max = (list) => Math.max(...list);
+
+const min = (list) => list.sort((a,b) => a - b)[0];
+
+const max = (list) => list.sort((a,b) => b - a)[0];
+
+
+console.log(min([-52, 56, 30, 29, -54, 0, -110]));
+console.log(max([-52, 56, 30, 29, -54, 0, -110]));
